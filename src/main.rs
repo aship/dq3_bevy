63785362cc6f104b3d503e428279535d72f65814
Yasuo Ashina <ashipp3000@gmail.com>
2024-04-17 16:58:45 +0900

    set background color

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use bevy::prelude::*;
 
-fn main() {
-    println!("Hello, bevy world!");
+const BACKGROUND_COLOR: Color = Color::rgb(0.68, 0.87, 0.89);
 
+fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_systems(Startup, setup)
         .run();
+}
+
+// Add the game's entities to our world
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
 }
